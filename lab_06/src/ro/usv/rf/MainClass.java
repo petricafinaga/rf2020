c4aa4b895f8bbd89d85c01b2a68393bc4fcc9e57
petricafinaga <petricafinaga@gmail.com>
2020-04-27 12:42:20 +0300

    Lab_06

---
+++ lab_06/src/ro/usv/rf/MainClass.java
@@ -0,0 +1,50 @@
+package ro.usv.rf;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		String[][] learningSet;
+		String[][] evaluationSet;
+
+		try {
+			learningSet = FileUtils.readCsvSetFromFile("gradesClasses.txt");
+			evaluationSet = FileUtils.readCsvSetFromFile("in.txt");
+
+			int numberOfPatterns = learningSet.length;
+			int numberOfFeatures = learningSet[0].length;
+			System.out.println(String.format("The learning set has %s patters and %s features", numberOfPatterns, numberOfFeatures));
+
+			double[][] distMatrix = Utils.calculateEuclidianGeneralizedDistance(evaluationSet, learningSet);
+
+			Queue<Element> pq = new PriorityQueue<>();
+			int[] nn = { 1, 3, 5, 7, 9, 13, 17 };
+
+			int nameIndex = numberOfFeatures - 1;
+			for (int i = 0; i < evaluationSet.length; ++i) {
+				for (int j = 0; j < numberOfPatterns ; ++j) {
+					pq.add(new Element(distMatrix[i][j], learningSet[j][nameIndex]));
+				}
+
+				System.out.print("Pattern " + evaluationSet[i][0] + ": ");
+				for (int n : nn) {
+					Queue<Element> tempQ = new PriorityQueue<>(pq);
+					System.out.print(Utils.NNMethod(tempQ, n) + " ");
+				}
+
+				System.out.println();
+				pq.clear();
+			}
+
+		} catch (USVInputFileCustomException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			System.out.println("Finished learning set operations");
+		}
+	}
+	
+
+
+}
---
+++ lab_06/src/ro/usv/rf/Utils.java
@@ -0,0 +1,68 @@
+package ro.usv.rf;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Queue;
+
+class Element implements Comparable<Element>{
+
+	@Override
+	public int compareTo(Element elem) {
+		if (this.distance > elem.distance) return 1;
+		else if (this.distance < elem.distance) return -1;
+		else return 0;
+	}
+
+	public Element(double dist, String name) {
+		this.distance = dist;
+		this.name = name;
+	}
+
+	double distance;
+	String name;
+}
+
+public class Utils {
+
+	 protected static String NNMethod(Queue<Element> pq, int n) {
+		 Map<String, Integer> map = new HashMap<>();
+		 for (int i = 0; i < n; ++i) {
+			 map.merge(pq.poll().name, 1, Integer::sum);
+		 }
+
+		 int max = 0;
+		 String className = "";
+
+		 for(Entry<String, Integer> entry: map.entrySet()) {
+			 if(entry.getValue() > max) {
+				 max = entry.getValue();
+				 className = entry.getKey();
+			 }
+		 }
+
+		 return className;
+	 }
+
+	protected static double[][] calculateEuclidianGeneralizedDistance(String[][] evaluationSet, String[][] learningSet) {
+
+		int evalLength = evaluationSet.length;
+		int learningLength = learningSet.length;
+		double[][] matrix = new double[evalLength][learningLength];
+
+		for (int i = 0; i < evalLength; i++) {
+			for (int j = 0; j < learningLength; j++) {
+				matrix[i][j] = calculateEuclidianDistance(evaluationSet[i], learningSet[j]);
+			}
+		}
+
+		return matrix;
+	}
+
+	private static double calculateEuclidianDistance(String[] a, String[] b) {
+		double sum = 0.0;
+		sum = Math.pow(Double.valueOf(a[0]) - Double.valueOf(b[0]), 2);
+
+		return Math.floor(Math.sqrt(sum) * 100) / 100;
+	}
+}
