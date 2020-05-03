4524fb1bd15574dfe5c8149afe7366330ee390f8
petricafinaga <petricafinaga@gmail.com>
2020-05-03 21:19:13 +0300

    Finish lab_09

--- lab_09/src/ro/usv/rf/MainClass.java
+++ lab_09/src/ro/usv/rf/MainClass.java
@@ -1,5 +1,7 @@
 package ro.usv.rf;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainClass {
@@ -12,14 +14,17 @@ public class MainClass {
 			int numberOfPatterns = evaluationSet.length;
 			int numberOfFeatures = evaluationSet[0].length;
 			System.out.println(String.format("The evaluation set has %s patters and %s features", numberOfPatterns, numberOfFeatures));
+			
+			List<Point> points = new ArrayList<>();
+			for (int i = 0; i < evaluationSet.length; ++i) {
+				points.add(new Point(evaluationSet[i][0], evaluationSet[i][1]));
+			}
 
+			int[] classification = Utils.dynamicKernels(points, 2);
 			
-
-
-			
-			
-			
-			
+			for (int i = 0; i < classification.length; ++i) {
+				System.out.print(classification[i] + " ");
+			}
 
 		} catch (USVInputFileCustomException e) {
 			System.out.println(e.getMessage());
--- lab_09/src/ro/usv/rf/Utils.java
+++ lab_09/src/ro/usv/rf/Utils.java
@@ -1,32 +1,111 @@
 package ro.usv.rf;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class Utils {
-
-	protected static double calculateEuclidianDistance(double[] a, double[] b) {
-		return Math.sqrt(Math.pow(b[0] - a[0], 2) + Math.pow(b[1] - a[1], 2));
+class Point {
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void plus(Point p) {
+		this.x += p.x;
+		this.y += p.y;
+	}
+	
+	public Point div(int x) {
+		this.x /= x;
+		this.y /= y;
+		
+		return this;
 	}
 
-	protected static double[] dynamicKernels(double[][] evaluationSet, int m) {
-		int numberOfPatterns = evaluationSet.length;
-		int numberOfFeatures = evaluationSet[0].length;
+	double x;
+	double y;
+}
 
-		double[] iClass = new double[numberOfPatterns];
-		double[][] kernels = new double[m][numberOfFeatures];
-				
-//		kernels[0] = evaluationSet[rand.nextInt(numberOfPatterns)];
-//		kernels[1] = evaluationSet[rand.nextInt(numberOfPatterns)];
-		kernels[0] = evaluationSet[0];
-		kernels[1] = evaluationSet[1];
+public class Utils {
+	static Random rand = new Random();
+	static int[] classes;
 
-		boolean done = true;
+	protected static int[] dynamicKernels(List<Point> points, int m) {
+		int nrOfPoints = points.size();
+		List<Point> kernels = initializeKernels(points, m);
+
+		classes = new int[nrOfPoints];
+
+		boolean done = false;
 		do {
+			done = true;
+			int[] classMembers = new int[m];
+			Point[] gravityCenters = new Point[m];
+			for (int i = 0; i < m; ++i) {
+				gravityCenters[i] = new Point(0,0);
+			}
+
+			for (int i = 0; i < nrOfPoints; ++i) {
+				double minDistance = Double.MAX_VALUE;
+				int classIndex = 0;
+				for (int j = 0; j < kernels.size(); ++j) {
+					double dist = calculateEuclidianDistance(points.get(i), kernels.get(j));
+					if (dist < minDistance) {
+						minDistance = dist;
+						classIndex = j;
+					}
+				}
+
+				if (classes[i] != classIndex) {
+					done = false;
+				}
+
+				gravityCenters[classIndex].plus(points.get(i));
+				classes[i] = classIndex;
+				classMembers[classIndex]++;
+			}
+			System.out.println("1");
+			if (!done) {
+				for (int i = 0; i < m; ++i) {
+					Point centroid = getCentroid(points, i, gravityCenters[i].div(classMembers[i]));
+					kernels.set(i, centroid);
+//					kernels.set(i, gravityCenters[i].div(classMembers[i]));
+				}
+			}
 			
 		} while(!done); 
 
-		return iClass;
+		return classes;
 	}
 	
+	//get closest point to gravity center from all points that has the same class
+	private static Point getCentroid(List<Point> points, int classIndex, Point gravityCenter) {
+		double minDist = Double.MAX_VALUE;
+		Point closestPoint = new Point(0, 0);
+		for (int i = 0; i < points.size(); ++i) {
+			if (classes[i] == classIndex) {
+				double dist = calculateEuclidianDistance(points.get(i), gravityCenter);
+				
+				if (dist < minDist) {
+					minDist = dist;
+					closestPoint = points.get(i);
+				}
+			}
+		}
+		
+		return closestPoint;
+	}
 
+	private static List<Point> initializeKernels(List<Point> points, int k) {
+		List<Point> kernels = new ArrayList<Point>();
+		
+		for (int i = 0; i < k; ++i) {
+			kernels.add(points.get(rand.nextInt(points.size())));
+		}
+		return kernels;
+	}
+
+	private static double calculateEuclidianDistance(Point a, Point b) {
+		return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
+	}
 }
